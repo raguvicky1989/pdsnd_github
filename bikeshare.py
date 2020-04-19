3f623a7e05cb7f397466b84a9c92d5dc4c0912a5
vignesh <vigneshragunathg@gmail.com>
2020-04-19 19:31:18 +0530

    Comment all menthos other than load_data in main method

--- bikeshare.py
+++ bikeshare.py
@@ -222,11 +222,11 @@ def main():
         print(city, month, day)
         df = load_data(city, month, day)
 
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df, city)
-        display_data(df)
+        #time_stats(df)
+        #station_stats(df)
+        #trip_duration_stats(df)
+        #user_stats(df, city)
+        #display_data(df)
        
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':

058d471f46a87de865957314b6c6d6ea9483a242
vignesh <vigneshragunathg@gmail.com>
2020-04-19 19:32:37 +0530

    un-comment display_data function in main method

--- bikeshare.py
+++ bikeshare.py
@@ -226,7 +226,7 @@ def main():
         #station_stats(df)
         #trip_duration_stats(df)
         #user_stats(df, city)
-        #display_data(df)
+        display_data(df)
        
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
